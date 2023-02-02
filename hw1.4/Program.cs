f1625cc6c0cae76ffcc8b3e847fcd08232748351
George Shartner <shartnerg@gmail.com>
2023-02-02 03:00:42 +0200

    1.4

---
+++ hw1.4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter a number N:");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("The even numbers from 1 to N:");
+for (int i = 2; i <= n; i += 2)
+{
+Console.WriteLine(i);
+}

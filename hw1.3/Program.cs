a057230177acc001452ae7b7c028a7d9dea82da6
George Shartner <shartnerg@gmail.com>
2023-02-02 02:56:37 +0200

    1.3

---
+++ hw1.3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter a number:");
+int num = int.Parse(Console.ReadLine());
+            
+if (num % 2 == 0)
+{
+Console.WriteLine("The number is even.");
+}
+else
+{
+Console.WriteLine("The number is odd.");
+}

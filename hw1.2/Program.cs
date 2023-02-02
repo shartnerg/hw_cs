954ba679088089cd2db870c5181b14ef1b81c8ef
George Shartner <shartnerg@gmail.com>
2023-02-02 02:49:32 +0200

    hw1.2

---
+++ hw1.2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter three numbers: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max)
+{
+ max = num2;
+}
+
+if (num3 > max)
+{
+max = num3;
+}
+
+Console.WriteLine("The maximum number is: " + max);
+Console.ReadLine();

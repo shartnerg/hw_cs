a1c62fd45c0d66a65ba0264480d2d6f668aa8c07
George Shartner <shartnerg@gmail.com>
2023-02-02 02:23:11 +0200

    hw1.1

---
+++ hw1.1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.Write("First number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Second number: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n>m)
+{
+    Console.Write($"max: {n} ");
+    Console.Write($"min: {m} ");
+}
+else if (n<m)
+{
+    Console.Write($"max: {m} ");
+    Console.Write($"min: {n} ");
+}
+else if (n==m)
+{
+    Console.Write("numbers are equal");
+}

67aa298a1606f78ddde2badc4a2c90f988f57fff
George Shartner <shartnerg@gmail.com>
2023-02-07 22:04:04 +0200

    2.1

---
+++ hw2.1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.Write("Vvedite 3x znachnoe chislo: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n<1000 & n>100 || n>-1000 & n<-100)
+{
+int n1 = (n / 10)%10;
+Console.WriteLine(Math.Abs(n1));
+}
+else 
+Console.WriteLine("Nuzhno vvseti 3x znachnoe chislo");

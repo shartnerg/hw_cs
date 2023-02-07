9c21e64dd32ee670bebccb0b2ccec046dc59b6a2
George Shartner <shartnerg@gmail.com>
2023-02-07 22:44:40 +0200

    2.2

---
+++ hw2.2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Write number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while(n>1000 || n<-1000)
+n=n/10;
+if(n>100 || n<-100)
+{
+int n1 = n%10;
+Console.WriteLine(Math.Abs(n1));
+}
+else 
+Console.WriteLine("Tretey cifri net");

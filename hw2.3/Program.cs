feba5daf2ee6eb8dac90cadb970928e5391a04d3
George Shartner <shartnerg@gmail.com>
2023-02-07 23:04:47 +0200

    2.3

---
+++ hw2.3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Write day number of the week(1-7): ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 8 & n > 0)
+{
+    if (n > 5 & n < 8)
+        Console.WriteLine("yes");
+    else
+        Console.WriteLine("no");
+}
+else Console.WriteLine("You need to write 1-7 number");

fdf5c6afc0b1e56ee97c1c61f53c6be7a57ab5c4
George Shartner <shartnerg@gmail.com>
2023-02-09 14:17:21 +0200

    3.1

--- hw3.1/Program.cs
+++ hw3.1/Program.cs
@@ -2,13 +2,13 @@
 
 Console.Write("Vvedite 5znachnoe chislo: ");
 int n = Convert.ToInt32(Console.ReadLine());
-while (Math.Abs(n) < 10000 || Math.Abs(n) >= 100000)
+while (n < 10000 || n >= 100000)
 {
-    Console.Write("Nuzhno vvesti 5znachnoe chislo!\nVvedite 5znachnoe chislo: ");
+    Console.Write("Nuzhno vvesti 5znachnoe polozhitelnoe chislo!\nVvedite 5znachnoe polozhitelnoe chislo: ");
     n = Convert.ToInt32(Console.ReadLine());
 }
 int m = n / 10000, i = n % 10000 / 1000, k = n % 100 / 10;
-if (Math.Abs(m) + Math.Abs(i) == Math.Abs(n % 10) + Math.Abs(k))
+if (m + i == n % 10 + k)
     Console.WriteLine("eto palindrom");
 else
     Console.WriteLine("eto NE palindrom");

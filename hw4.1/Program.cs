d7b9342b95964353940f27b0474c347f047cedc7
George Shartner <shartnerg@gmail.com>
2023-02-26 23:42:55 +0200

    4.1

---
+++ hw4.1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+
+for (int i = 1; i <= b; i++)
+{
+    result *= a;
+}
+
+Console.WriteLine($"{a}^{b} = {result}");

ef5b30905b9d4743694343cbe130396904d23b09
George Shartner <shartnerg@gmail.com>
2023-02-26 23:47:49 +0200

    4.2

---
+++ hw4.2/Program.cs
@@ -0,0 +1,11 @@
+﻿ Console.Clear();
+ Console.Write("Введите число: ");
+ int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+        while (number != 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        Console.WriteLine($"Сумма цифр введенного числа: {sum}");

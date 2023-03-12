a82e3ae1967b9f205c78bf02569669cfe3934c0f
George Shartner <shartnerg@gmail.com>
2023-03-12 21:22:41 +0200

    5.1

---
+++ hw5.1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Vvedite razmer massiva: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(100, 1000); // [100; 999]
+Console.WriteLine($"Massiv: [{string.Join(", ", array)}]");
+int count = 0;
+foreach(int element in array)
+{
+    if(element%10==0 ||element%10==2|| element%10==4|| element%10==6|| element%10==8)
+        count=count+1;
+}
+Console.WriteLine($"Summa {count}");

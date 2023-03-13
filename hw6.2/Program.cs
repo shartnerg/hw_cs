5750f168d7dca985534ba1692f488289dad8bc7f
George Shartner <shartnerg@gmail.com>
2023-03-13 20:37:16 +0200

    6.2

---
+++ hw6.2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Vvedite k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Vvedite b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Vvedite k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Vvedite b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine("Tochka peresecheniya: ({0}, {1})", x, y);

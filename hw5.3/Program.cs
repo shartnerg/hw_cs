362fd1a44e949d8095ba110b3c131839d63e8fc6
George Shartner <shartnerg@gmail.com>
2023-03-13 16:16:35 +0200

    5.3

--- hw5.3/Program.cs
+++ hw5.3/Program.cs
@@ -5,15 +5,15 @@ int[] array = new int[n];
 for (int i = 0; i < array.Length; i++)
     array[i] = new Random().Next();
 Console.WriteLine($"Massiv: [{string.Join(", ", array)}]");
-double max=array[0];
+double max = array[0];
 for (int i = 1; i < array.Length; i++)
-    if(array[i]>max)
-        max=array[i];
+    if (array[i] > max)
+        max = array[i];
 Console.WriteLine($"max: {max}");
-double min=array[0];
+double min = array[0];
 for (int i = 1; i < array.Length; i++)
-    if(array[i]<min)
-        min=array[i];
+    if (array[i] < min)
+        min = array[i];
 Console.WriteLine($"min: {min}");
-double r=(max)-(min);
+double r = (max) - (min);
 Console.WriteLine($"Raznica max-min: {r}");

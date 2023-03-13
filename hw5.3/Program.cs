8e534d855f50c3eb56242fc8071816691a94d410
George Shartner <shartnerg@gmail.com>
2023-03-13 16:16:07 +0200

    5.3

--- hw5.3/Program.cs
+++ hw5.3/Program.cs
@@ -3,17 +3,17 @@ Console.Write("Vvedite razmer massiva: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[n];
 for (int i = 0; i < array.Length; i++)
-    array[i] = new Random().Next(-100,101);
+    array[i] = new Random().Next();
 Console.WriteLine($"Massiv: [{string.Join(", ", array)}]");
-int max=array[0];
+double max=array[0];
 for (int i = 1; i < array.Length; i++)
     if(array[i]>max)
         max=array[i];
 Console.WriteLine($"max: {max}");
-int min=array[0];
+double min=array[0];
 for (int i = 1; i < array.Length; i++)
     if(array[i]<min)
         min=array[i];
 Console.WriteLine($"min: {min}");
-int r=max-min;
+double r=(max)-(min);
 Console.WriteLine($"Raznica max-min: {r}");

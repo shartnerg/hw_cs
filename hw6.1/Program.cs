02b35d857568ace0032c67c09ff763ae0921a864
George Shartner <shartnerg@gmail.com>
2023-03-13 19:11:26 +0200

    6.1

---
+++ hw6.1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Vvedite chisla cherez probel: ");
+string numbersStr = Console.ReadLine();
+
+string[] numbers = numbersStr.Split(' ');
+int count = 0;
+
+foreach (string numberStr in numbers)
+{
+    int number = int.Parse(numberStr);
+    if (number > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Kolvo chisel bolshe 0: {count}");

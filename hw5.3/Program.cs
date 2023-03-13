5bfb006ffdd70f52eecb674c9605c6c4a7107094
George Shartner <shartnerg@gmail.com>
2023-03-13 15:39:12 +0200

    5.3

--- hw5.3/Program.cs
+++ hw5.3/Program.cs
@@ -6,26 +6,15 @@ for (int i = 0; i < array.Length; i++)
     array[i] = new Random().Next();
 Console.WriteLine($"Massiv: [{string.Join(", ", array)}]");
 for (int i = 0; i < array.Length; i++)
-    if (array[i+1]>array[i])
+    if (array[i + 1] > array[i])
     {
-    int max=array[i+1];
-    int min=array[i];
+        int max = array[i + 1];
+        int min = array[i];
     }
     else
-     if (array[i]>array[i+1])
+     if (array[i] > array[i + 1])
     {
-    int max=array[i];
-    int min=array[i+1];
+        max = array[i];
+        min = array[i + 1];
     }
 Console.Write($"Min, Max: [{string.Join(", ", min, max)}]");
-
-
-/*
-double r = 0;
-
-for (int i = 1; i < array.Length; i = i++)
-{
-    if(array[i+1]>array[i])
-    
-}
-Console.WriteLine($"Summa {count}");

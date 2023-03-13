6a01a32ab898520ffb06dc7be66c1166d1e70130
George Shartner <shartnerg@gmail.com>
2023-03-13 15:41:48 +0200

    5.3

--- hw5.3/Program.cs
+++ hw5.3/Program.cs
@@ -8,13 +8,14 @@ Console.WriteLine($"Massiv: [{string.Join(", ", array)}]");
 for (int i = 0; i < array.Length; i++)
     if (array[i + 1] > array[i])
     {
-        int max = array[i + 1];
-        int min = array[i];
+        int a = array[i + 1];
+        int b = array[i];
     }
     else
      if (array[i] > array[i + 1])
     {
-        max = array[i];
-        min = array[i + 1];
+       int a = array[i];
+       int b = array[i + 1];
     }
-Console.Write($"Min, Max: [{string.Join(", ", min, max)}]");
+Console.Write($"Min {a}");
+Console.Write($"Min {b}");

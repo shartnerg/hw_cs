07d1d83fe1be7f36668407c7aac8860bffb657d9
George Shartner <shartnerg@gmail.com>
2023-03-16 00:16:48 +0200

    9.2

---
+++ hw9.2/Program.cs
@@ -0,0 +1,14 @@
+﻿int rec(int a, int b)
+{
+    if (b == a)
+        return a;
+    return rec(a, b - 1) + b;
+}
+
+
+Console.Clear();
+Console.Write("Vvedite 1 chislo: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Vvedite 2 chislo: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(m, n));

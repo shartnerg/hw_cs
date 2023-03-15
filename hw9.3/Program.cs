e8ea3f5b9f990fdb0e2e345716c715e2250a1d71
George Shartner <shartnerg@gmail.com>
2023-03-16 00:28:55 +0200

    9.3

---
+++ hw9.3/Program.cs
@@ -0,0 +1,17 @@
+﻿int rec(int m, int n)
+{
+    if (m == 0)
+        return n+1;
+    else if (n == 0)
+        return rec(m-1, 1);
+    else
+    return rec(m-1, rec(m,n-1));
+}
+
+
+Console.Clear();
+Console.Write("Vvedite 1 chislo: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Vvedite 2 chislo: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(m, n));

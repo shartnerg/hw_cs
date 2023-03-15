cfa3f644506df06c749ede069f30f304a192a15f
George Shartner <shartnerg@gmail.com>
2023-03-16 00:08:26 +0200

    9.1

---
+++ hw9.1/Program.cs
@@ -0,0 +1,12 @@
+﻿string rec(int n)
+{
+    if (n == 1)
+        return "1";
+    return $"{n}, "+ rec(n - 1);
+}
+
+
+Console.Clear();
+Console.Write("Vvedite chislo: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n));
